3dd78fdcad101feb05163139a6888f0d0c14ea07
Dawson Richey <dawsonrichey@gmail.com>
2018-06-19 03:06:44 -0400

    Added Context

---
+++ src/OnlineGroceryAppModel/OnlineGroceryAppModel/Context.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OnlineGroceryAppModel.Models;
+
+namespace OnlineGroceryAppModel
+{
+	public class Context : DbContext
+	{
+		public DbSet<OnlineGrocery> OnlineGroceries { get; set; }
+ 	}
+}
