bc6e55d5b0643cef35c109ffe38b8cdd16239d3c
Dawson Richey <dawsonrichey@gmail.com>
2018-06-19 03:02:10 -0400

    Initial commit

---
+++ src/OnlineGroceryAppModel/OnlineGroceryAppModel/Models/OnlineGrocery.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineGroceryAppModel.Models
+{
+	public class OnlineGrocery
+	{
+		// ID, OnlineGroceryId, OnlineGroceryID
+		public int Id { get; set; }
+		public string FoodName { get; set; }
+		public int FoodCost { get; set; }
+		public string FoodType { get; set; }
+		public int FoodQuantity { get; set; }
+		public DateTime PublishedOn { get; set; }
+	}
+}
